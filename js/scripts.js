c45990adef7c9bc6bff6d539c40f43076b99b0d8
HenryEBG <berganza386@gmail.com>
2024-05-14 19:38:27 -0400

    working the initial fetch and the template

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,57 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+//objects to be modify
+const productContainer = document.getElementById("productContainer");
+
+function productdisplay(products) {
+  products.forEach(product => {
+    const card = document.createElement('div');
+    card.classList.add("col")
+    card.classList.add("mb-5")
+    card.innerHTML = `<div class="card h-100">
+    <!-- Sale badge-->
+    <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+    <!-- Product image-->
+    <img class="card-img-top h-50" src=${product.image} alt="..."  />
+    <!-- Product details-->
+    <div class="card-body p-4">
+        <div class="text-center">
+            <!-- Product name-->
+            <h5 class="fw-bolder">${product.title}</h5>
+            <!-- Product reviews-->
+            <div class="d-flex justify-content-center small text-warning mb-2">
+                <div class="bi-star-fill"></div>
+                <div class="bi-star-fill"></div>
+                <div class="bi-star-fill"></div>
+                <div class="bi-star-fill"></div>
+                <div class="bi-star-fill"></div>
+            </div>
+            <!-- Product price-->
+            $${product.price}
+        </div>
+    </div>
+    <!-- Product actions-->
+    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent d-flex justify-content-center ">
+        <div class="text-center"><a class="btn btn-outline-dark mt-auto ml-2" href="#">VO</a></div>
+        <div class="text-center"><a class="btn btn-outline-dark mt-auto ml-2" href="#">C</a></div>
+    </div>
+</div>`
+productContainer.appendChild(card);
+  });
+}
+//create the fetch
+async function getProducts() {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    console.log(data)
+    productdisplay(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getProducts();
+
